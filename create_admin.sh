ce17f3df54b94c987468ad3caa67c509acf2300a
Gustav Johansson <gustav@neer.se>
2023-02-03 15:20:45 +0100

    Trying to automate admin user.

--- create_admin.sh
+++ create_admin.sh
@@ -19,9 +19,15 @@ printf "${MAGENTA}"
 message "Creating admin user!"
 printf "${NC}"
 
-docker exec sharelatex /bin/bash -c "cd /var/www/sharelatex; grunt user:create-admin --email=admin@domain.com"
+regLink=$(docker exec sharelatex /bin/bash -c "cd /var/www/sharelatex; grunt user:create-admin --email=admin@domain.com" | grep http | sed 's#localhost#localhost:888#g')
+echo "${regLink}"
+token=$(echo "${regLink}" | awk -F'?' '{print $2}' | awk -F'&' '{print $1}' | awk -F'=' '{print $2}')
+echo "${token}"
+
+# curl -vvv ${regLink}
+curl -vvv --data 'password=very1Bad#' --data 'email=admin@domain.com' ${regLink} 
 
 # Exit
 printf "${MAGENTA}"
-message "All done!"
+message "All done! Login at http://localhost:888/login with username 'admin@domain.com' and password 'very1Bad#'"
 printf "${NC}"

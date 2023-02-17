ac3d39c15ede3c7aa1431935a3b27dd87dab13a1
Gustav Johansson <gustav@neer.se>
2023-02-17 11:33:17 +0100

    Added token handling on button click.

--- services/web/frontend/js/features/sign/components/pdf-sign-button.tsx
+++ services/web/frontend/js/features/sign/components/pdf-sign-button.tsx
@@ -4,8 +4,13 @@ import { useCallback } from 'react'
 import Tooltip from '../../../shared/components/tooltip'
 import Icon from '../../../shared/components/icon'
 import { useDetachCompileContext as useCompileContext } from '../../../shared/context/detach-compile-context'
+import { useState, useEffect } from 'react';
 
+// Get the refresh token from the environment.
 const ZohoRefreshToken = process.env.ZOHO_REFRESH_TOKEN
+const ZohoClientID = process.env.ZOHO_CLIENT_ID
+const ZohoClientSecret = process.env.ZOHO_CLIENT_SECRET
+const [ZohoAccessToken, setZohoAccessToken ] = useState('');
 
 function PdfSignButton() {
   const { pdfUrl } = useCompileContext()
@@ -15,9 +20,13 @@ function PdfSignButton() {
     ? t('sign_pdf')
     : t('please_compile_pdf_before_signing')
 
-  const handleSignButtonClick = useCallback(() => {
+  const handleSignButtonClick = useCallback(async () => {
+    // Debugging first.
     console.log("Clicked sign!");
-    console.log("Token is " + ZohoRefreshToken)
+    console.log("Refresh token is " + ZohoRefreshToken)
+    // Get the access token.
+    await getAccessToken();
+    console.log("Access token is " + ZohoAccessToken)
   })
 
   return (
@@ -37,4 +46,23 @@ function PdfSignButton() {
   )
 }
 
+// Function to retrieve a new access token since they are only valid for 1 h. 
+async function getAccessToken() {
+  let response = await fetch('https://accounts.zoho.eu/oauth/v2/auth', {
+    method: 'POST',
+    body: JSON.stringify({
+      grant_type: refresh_token,
+      refresh_token: ZohoRefreshToken,
+      client_id: ZohoClientID,
+      client_secret: ZohoClientSecret,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  let data = await response.json();
+  setZohoAccessToken(data.access_token);
+};
+
+// Publish the button.
 export default PdfSignButton
